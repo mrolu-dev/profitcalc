b5c14d86af6f29863ae2d50a2e68f06a5dfac626
ristabel <112583477+ristabel@users.noreply.github.com>
2023-06-28 13:53:37 -0600

    Update server.js

--- app/server.js
+++ app/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-require('./src/utils/db');
+// require('./src/utils/db');
 
 // Routes
 app.use('/api/calculator', calculatorRoutes);
