b878f128d1344bdb1f27dd35ecef3ea535d9b048
ristabel <112583477+ristabel@users.noreply.github.com>
2023-07-07 15:36:52 -0600

    Create Items.js

---
+++ Items.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+  costPrice: { type: Number, required: true },
+  sellingPrice: { type: Number, required: true },
+  taxRate: { type: Number, required: true },
+  shippingCost: { type: Number, required: true },
+  profit: { type: Number, required: true },
+  expenses: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Item', itemSchema);
