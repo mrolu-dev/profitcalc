176a7d1703a11533c5908dfa1aabb8673c7d5483
ristabel <112583477+ristabel@users.noreply.github.com>
2023-07-07 08:48:06 -0600

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const calculatorRoutes = require('./app/src/routes/calculatorRoutes');
+const calculatorRoutes = require('./src/routes/calculatorRoutes');
 
 const app = express();
 const port = 3000;
