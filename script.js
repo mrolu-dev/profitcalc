a1eac641ff36639e9b33aed77c88e992f13749c2
ristabel <112583477+ristabel@users.noreply.github.com>
2023-07-07 16:24:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,36 +6,40 @@ document.getElementById('calculatorForm').addEventListener('submit', (e) => {
   const taxRate = parseFloat(document.getElementById('taxRate').value);
   const shippingCost = parseFloat(document.getElementById('shippingCost').value);
 
-// Perform calculations or API requests to obtain the result
+  // Perform calculations or API requests to obtain the result
   const profit = sellingPrice - costPrice - shippingCost;
   const expenses = costPrice + shippingCost;
 
   // Redirect to the result page with query parameters
   const resultUrl = `result.html?profit=${profit}&expenses=${expenses}`;
   window.location.href = resultUrl;
-  
-  const payload = { costPrice, sellingPrice, taxRate, shippingCost };
-
-  fetch('/api/calculator/calculate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.success) {
-        document.getElementById('result').innerHTML = `
-          <p>Profit: $${data.newItem.profit}</p>
-          <p>Expenses: $${data.newItem.expenses}</p>
-        `;
-      } else {
-        document.getElementById('result').innerHTML = `<p>Failed to calculate. Please try again.</p>`;
-      }
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      document.getElementById('result').innerHTML = `<p>An error occurred. Please try again.</p>`;
-    });
 });
+
+// The code below will be moved to the result.html page or result.js file
+
+/*
+const payload = { costPrice, sellingPrice, taxRate, shippingCost };
+
+fetch('/api/calculator/calculate', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(payload),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.success) {
+      document.getElementById('result').innerHTML = `
+        <p>Profit: $${data.newItem.profit}</p>
+        <p>Expenses: $${data.newItem.expenses}</p>
+      `;
+    } else {
+      document.getElementById('result').innerHTML = `<p>Failed to calculate. Please try again.</p>`;
+    }
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+    document.getElementById('result').innerHTML = `<p>An error occurred. Please try again.</p>`;
+  });
+*/
