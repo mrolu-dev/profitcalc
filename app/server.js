fd40a424b490fa28eff39b59024d2ac51fc85159
ristabel <112583477+ristabel@users.noreply.github.com>
2023-07-07 15:29:07 -0600

    Update server.js

--- app/server.js
+++ app/server.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 
 // Root URL handler
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/views/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 
