adb04dc70a5ccd6f764d75ece5ad0bc2a7ccc4e8
ristabel <112583477+ristabel@users.noreply.github.com>
2023-07-07 15:37:55 -0600

    Create calculatorController.js

---
+++ calculatorController.js
@@ -0,0 +1,44 @@
+const Item = require('../models/Item');
+const calculateProfit = require('../utils/calculateProfit');
+
+module.exports = {
+  calculate: async (req, res) => {
+    const { costPrice, sellingPrice, taxRate, shippingCost } = req.body;
+
+   
+    // Calculate profit and expenses
+    const { profit, expenses } = calculateProfit(costPrice, sellingPrice, taxRate, shippingCost);
+  try {
+    // Create a new item in the database
+    const newItem = new Item({
+      costPrice,
+      sellingPrice,
+      taxRate,
+      shippingCost,
+      profit,
+      expenses,
+    });
+    // Save the new item
+      await newItem.save();
+    
+     // Redirect to the result page
+      res.redirect('/result');
+    } catch (err) {
+      console.error('Failed to save item:', err);
+      res.status(500).json({ success: false, message: 'Failed to save item' });
+    }
+  },
+
+  displayResult: async (req, res) => {
+    try {
+      // Retrieve the calculation result from the database
+      const resultItem = await Item.findOne().sort({ _id: -1 }).exec();
+      // Render the result page with the calculation result
+      res.render('result', { resultItem });
+   
+    } catch (err) {
+      console.error('Failed to save item:', err);
+      res.status(500).json({ success: false, message: 'Failed to save item' });
+    }
+  },
+};
