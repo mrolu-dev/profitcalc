f7d8cc855f6d3812258a9647bb0a92959ef5cddf
ristabel <112583477+ristabel@users.noreply.github.com>
2023-07-07 00:46:00 -0600

    Update server.js

--- app/server.js
+++ app/server.js
@@ -9,14 +9,18 @@ const port = 3000;
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+console.log("Middleware configured successfully.");
 
 // Connect to MongoDB
+console.log("Connecting to MongoDB...");
 require('./src/utils/db');
+
 
 // Routes
 app.use('/api/calculator', calculatorRoutes);
 
 // Start the server
+console.log(`Starting the server on port ${port}...`);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
