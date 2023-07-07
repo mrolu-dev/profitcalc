6c9883af27166390917edd11e91f89d7c078a5e8
ristabel <112583477+ristabel@users.noreply.github.com>
2023-07-07 15:39:11 -0600

    Create calculatorProfit.js

---
+++ calculatorProfit.js
@@ -0,0 +1,7 @@
+module.exports = (costPrice, sellingPrice, taxRate, shippingCost) => {
+  const expenses = costPrice + shippingCost;
+  const tax = (sellingPrice * taxRate) / 100;
+  const profit = sellingPrice - expenses - tax;
+
+  return { profit, expenses };
+};
