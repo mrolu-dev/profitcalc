fae04046a10dc2c1f9afd233eae6f227fdf02913
ristabel <112583477+ristabel@users.noreply.github.com>
2023-07-07 16:30:28 -0600

    Update result.js

--- result.js
+++ result.js
@@ -2,24 +2,14 @@
 // JavaScript code for the result.html page in the dropshipping calculator app
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Get the calculation result values from the HTML
-  const profitElement = document.getElementById('profit');
-  const expensesElement = document.getElementById('expenses');
+  const urlParams = new URLSearchParams(window.location.search);
+  const profit = urlParams.get('profit');
+  const expenses = urlParams.get('expenses');
 
-  // Extract the values from the HTML elements
-  const profit = parseFloat(profitElement.textContent.replace('$', ''));
-  const expenses = parseFloat(expensesElement.textContent.replace('$', ''));
-
-  // Example functionality: Update the styling based on the profit value
-  if (profit > 0) {
-    profitElement.style.color = 'green';
-  } else if (profit < 0) {
-    profitElement.style.color = 'red';
-  } else {
-    profitElement.style.color = 'black';
-  }
-
-  // Example functionality: Log the profit and expenses to the console
-  console.log('Profit:', profit);
-  console.log('Expenses:', expenses);
+  // Display the result in the div with id "result"
+  const resultDiv = document.getElementById('result');
+  resultDiv.innerHTML = `
+    <p>Profit: $${profit}</p>
+    <p>Expenses: $${expenses}</p>
+  `;
 });
