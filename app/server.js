538aa686b940496e8c38258ea276044ccb78aa67
ristabel <112583477+ristabel@users.noreply.github.com>
2023-07-07 14:35:36 -0600

    Update server.js

--- app/server.js
+++ app/server.js
@@ -6,15 +6,19 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+
+// Serve static files
+app.use(express.static("public"));
+
+// Root URL handler
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/views/index.html'));
+});
+
+
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-  // Logic to handle the root URL request
-  res.sendFile(path.join(__dirname, '/src/views/index.html'));
-});
-
 console.log("Middleware configured successfully.");
 
 // Connect to MongoDB
