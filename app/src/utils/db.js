089c7ce005d4dd5ad045467a9c2d6c4e30040fad
ristabel <112583477+ristabel@users.noreply.github.com>
2023-07-07 00:29:10 -0600

    Update db.js

--- app/src/utils/db.js
+++ app/src/utils/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose
-  .connect('mongodb+srv://<credentials>@cluster0.wlgzdod.mongodb.net/?appName=mongosh+1.10.1', {
+  .connect('mongodb+srv://ristabel:MonAvi1$@cluster0.wlgzdod.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
