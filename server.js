005629800dc8fe1cf3b7e37b7aed348bdad374d1
ristabel <112583477+ristabel@users.noreply.github.com>
2023-07-10 11:23:41 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const {ObjectId} = require('mongodb');
 const calculatorRoutes = require('./calculatorRoutes');
 const path = require('path');
 const app = express();
@@ -20,11 +21,18 @@ app.get('/result', (req, res) => {
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 console.log("Middleware configured successfully.");
 
 // Connect to MongoDB
 console.log("Connecting to MongoDB...");
 require('./db');
+
+// Start the server
+console.log(`Starting the server on port ${port}...`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 
 // Routes
@@ -32,9 +40,43 @@ console.log("Setting up routes...");
 app.use('/api/calculator/calculate', calculatorRoutes);
 console.log("Routes configured successfully.");
 
+app.get('/data', (req,res) =>{
+  let data =[]
+  db.collection('data')
+  .find() 
+  .sort()
+  .forEach(data => data.push(data))
+  .then(() => {
+    res.status(200).json(data)
+  })
+  .catch(() =>{
+    res.status(500).json({error: 'could not fetch the documents'})
+  })
 
-// Start the server
-console.log(`Starting the server on port ${port}...`);
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.get('/data/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)){
+    db.collection('data)
+      .findOne({_id:ObjectId(req.parrams.id)})
+      .then(doc =>{
+        res.status(200).json(doc)
+      })
+      .catch(err => {
+        res.status(500).json({error: 'could not fetch the documents'})
+    })
+                  }
+  else {
+    res.status(500).json({error: 'Not a valid doc id'})
+  }
+    })
+app.post('/data, req, res) => {   
+  const para = req.body
+
+  db.collection('data')
+    .insertOne(para)
+    .then(answer => {
+      res.status(201).json(answer)
+    })
+  .catch(err => {
+    res.status(500).json({error: 'could not create a new document'})
+  })
+})
